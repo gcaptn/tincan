95b0b4db45602e0440d2ba67d57a1e4533a843b0
gcaptn <gcaptain.t@gmail.com>
2021-05-14 21:27:54 +0700

    refactor test runner

--- mod.ts
+++ mod.ts
@@ -1,65 +1,20 @@
-import {
+import { runNode } from "./runner.ts";
+import { root } from "./nodes.ts";
+
+export async function run() {
+  const success = await runNode(root);
+  if (!success) {
+    Deno.exit(1);
+  }
+}
+
+export {
   afterAll,
   afterEach,
   beforeAll,
   beforeEach,
   describe,
-  DescribeNode,
   it,
-  ItNode,
-  root,
-  RootNode,
 } from "./nodes.ts";
-import { reportCase, reportEnd, reportStart } from "./report.ts";
-
-async function runNode(node: RootNode | DescribeNode | ItNode) {
-  if (node instanceof RootNode) {
-    // root node
-    reportStart(node);
-    node.isRunning = true;
-    const start = Date.now();
-    for (const hook of node.beforeAll) await hook();
-    for (const child of node.children) {
-      await runNode(child);
-      if (child.result === "FAIL") {
-        node.result = "FAIL";
-      }
-    }
-    for (const hook of node.afterAll) await hook();
-    node.timeTaken = Date.now() - start;
-    reportEnd(node);
-  } else if (node instanceof DescribeNode) {
-    // describe node
-    if (node.skipped) return;
-    for (const hook of node.beforeAll) await hook();
-    for (const child of node.children) {
-      await runNode(child);
-      if (child.result === "FAIL") {
-        node.result = "FAIL";
-      }
-    }
-    for (const hook of node.afterAll) await hook();
-  } else {
-    // it node
-    if (node.skipped) return;
-    for (const hook of node.beforeEach) await hook();
-    const start = Date.now();
-    try {
-      await node.fn();
-    } catch (err) {
-      node.result = "FAIL";
-      node.error = err;
-    }
-    node.timeTaken = Date.now() - start;
-    reportCase(node);
-    for (const hook of node.afterEach) await hook();
-  }
-}
-
-export async function run() {
-  await runNode(root);
-}
-
-export { afterAll, afterEach, beforeAll, beforeEach, describe, it };
 
 export * from "https://deno.land/x/expect/mod.ts";
